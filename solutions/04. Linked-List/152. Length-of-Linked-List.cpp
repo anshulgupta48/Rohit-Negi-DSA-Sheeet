c4c46a80a5d85d2e65cb4de9db00a5150f8aa5ef
anshulgupta48 <anshulgupta4905@gmail.com>
2026-03-09 21:08:16 +0530

    Day-50

---
+++ solutions/04. Linked-List/152. Length-of-Linked-List.cpp
@@ -0,0 +1,30 @@
+// ******** Given head of a singly linked list. The task is to find the length of the linked list, where length is defined as the number of nodes in the linked list. ********
+
+// <======== Example ========>
+// Input: 1 -> 2 -> 3 -> 4 -> 5 -> NULL
+// Output: 5
+
+// Input: 2 -> 4 -> 6 -> 7 -> 5 -> 1 -> 0 -> NULL
+// Output: 7
+
+
+// Expected Time Complexity ==> O(n)
+// Expected Auxiliary Space ==> O(1)
+
+
+
+
+class Solution{
+    public:
+    int getCount(Node* head) {
+        Node* temp = head;
+        int count = 0;
+        
+        while(temp != NULL) {
+            count++;
+            temp = temp->next;
+        }
+        
+        return count;
+    }
+};
